22a91a0845141615b31ee164004f8cd37e095bc7
Unknown <tristan@elantris.com>
2018-03-10 16:33:13 -0800

    Added Headings
    Headings added to style guide

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+$(function(){
+    
+    $(".brand-style").each(function(){
+        var text = $(this).text();
+        $(this).attr("data-content", text);
+        console.log(text);
+    });
+
+});
