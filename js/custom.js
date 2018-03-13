39f8c02afd11052dd029cd319e3401597268dc35
Unknown <tristan@elantris.com>
2018-03-13 15:43:36 -0700

    Updating Style Guide
    Colors, Project Listing, Citations, Headings

--- js/custom.js
+++ js/custom.js
@@ -5,5 +5,37 @@ $(function(){
         $(this).attr("data-content", text);
         console.log(text);
     });
+    
+//    
+//    $(".project-card").hover(function(){
+//       $( ".project-title" ).animate({
+//          right: "0%",
+//          opacity: "1",
+//        }, {
+//          duration: 250,
+//          queue: true
+//        }); 
+//        $( ".project-type" ).animate({
+//          right: "0%",
+//          opacity: "1",
+//        }, {
+//          duration: 250,
+//          queue: true
+//        }); 
+//        $( ".project-role" ).animate({
+//          right: "0%",
+//          opacity: "1",
+//        }, {
+//          duration: 250,
+//          queue: true
+//        }); 
+//        $( ".project-tech" ).animate({
+//          right: "0%",
+//          opacity: "1",
+//        }, {
+//          duration: 250,
+//          queue: true
+//        }); 
+//    });
 
 });
