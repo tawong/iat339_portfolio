3da9e66f616db362ac869353bdb7883b5d5e9b56
Unknown <tristan@elantris.com>
2018-04-04 14:38:59 -0700

    Cleanup + Updates
    Cleaned up CSS and HTML, updated favicon

--- js/custom.js
+++ js/custom.js
@@ -6,36 +6,6 @@ $(function(){
         console.log(text);
     });
     
-//    
-//    $(".project-card").hover(function(){
-//       $( ".project-title" ).animate({
-//          right: "0%",
-//          opacity: "1",
-//        }, {
-//          duration: 250,
-//          queue: true
-//        }); 
-//        $( ".project-type" ).animate({
-//          right: "0%",
-//          opacity: "1",
-//        }, {
-//          duration: 250,
-//          queue: true
-//        }); 
-//        $( ".project-role" ).animate({
-//          right: "0%",
-//          opacity: "1",
-//        }, {
-//          duration: 250,
-//          queue: true
-//        }); 
-//        $( ".project-tech" ).animate({
-//          right: "0%",
-//          opacity: "1",
-//        }, {
-//          duration: 250,
-//          queue: true
-//        }); 
-//    });
+
 
 });
